819acea7d6b2f770538ea49ab6f274b841b90dd7
Alexis <paulpulamarin141220@gmail.com>
2024-11-15 15:49:36 -0500

    Login completado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { initializeApp, provideFirebaseApp} from '@angular/fire/app';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import {environment} from '../environments/environment'
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(()=>getAuth()),
+    provideFirestore(()=>getFirestore()),
+    provideStorage(() => getStorage())
+    ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAJn_tGbz44coLI1vINPmbJMCOkaEBts5s",
+    authDomain: "balexis-8a07a.firebaseapp.com",
+    projectId: "balexis-8a07a",
+    storageBucket: "balexis-8a07a.firebasestorage.app",
+    messagingSenderId: "103493038467",
+    appId: "1:103493038467:web:46ec24b9a4c454083dc6b6",
+    measurementId: "G-GCW29L1C8H",
+  }
 };
